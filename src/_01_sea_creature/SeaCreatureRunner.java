5c4174a7cec4f9e599ca532ba2256639dee40a4f
student <student@ip-172-31-63-111.us-west-2.compute.internal>
2021-01-06 16:53:35 -0800

    Lesson 1 of Module 2

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,22 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+
+public static void main(String[] args) {
+
+	SeaCreature Spongebob = new SeaCreature("Spongebob"); 
+	
+	Spongebob.eat();
+	Spongebob.laugh();
+	
+	SeaCreature Patrick = new SeaCreature("Patrick");
+	
+	Patrick.eat();
+	Patrick.laugh();
+	
+	SeaCreature Squidward = new SeaCreature ("Squidward");
+	
+	Squidward.eat();
+	Squidward.laugh();
+}	
+}
